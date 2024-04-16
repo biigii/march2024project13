fdef810eb1bdf60091578d68b11da1d9b6747548
Behshad <82580731+biigii@users.noreply.github.com>
2024-04-16 19:34:43 +1200

    more js

--- main.js
+++ main.js
@@ -1,47 +1,13 @@
-// parent.child.grantdChild.greatGrandChild
-
-// var text = "hello world"
-// var num = 1000;
-
-// window.document
-// console.log(window.document)
-
-// in CSS we had background-color, but in Javascript we use
-// a syntax called camel casing ==> 
-// backgroundColor or textAlign marginRight
-// console.log('it works')
-// document.body.style.backgroundColor = "#f88";
-
-// document.getElementById("heading").style.color = "red";
-// document.getElementById("heading").style.fontSize = "5em";
-// document.getElementById("heading").style.textTransform = "uppercase"
 
 
 
-var myAwesomePhoto = document.getElementById("photo");
-// console.log(myAwesomePhoto)
-myAwesomePhoto.style.border = "20px solid red"
-myAwesomePhoto.style.rotate = "45deg"
+var mainHeading = document.createElement("h1");
+mainHeading.textContent = "homepage"
+document.body.appendChild(mainHeading);
 
-var wordOne = "hello"
-var wordTwo = "world"
-
-console.log(wordOne + wordTwo)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-myAwesomePhoto.style.borderRadius = "50%"
+var image = document.createElement('img')
+image.src = "https://picsum.photos/id/1050/300"
+image.alt = "my very awesome photo"
+image.style.borderRadius = "50%"
+image.style.border = "3px solid"
+document.getElementById("image-holder").appendChild(image)
