a6ca913ae5e33c67e689bdf2454bcfb1e345ba26
Behshad <82580731+biigii@users.noreply.github.com>
2024-04-16 18:43:29 +1200

    js

---
+++ main.js
@@ -0,0 +1,33 @@
+// () {} [] ; , "" ''
+// {
+//     ([])
+//     {
+//         ([])
+//     }
+// }
+
+// parent.child.grandchild
+
+// alert("hello world");
+// console.log which you use to log/print to your browser console
+// ; semicolon at the end of line is optional
+// console.log("Hello World")
+// console.log(1000) // number
+// console.log("1000") // string
+
+// console.log(1000 + 1000) // adding 2 numbers
+// console.log("1000" + "1000") // adding 2 strings
+
+// var age;
+// age = 10;
+// age = 100;
+
+// console.log(age)
+
+// var num1 = 1000;
+// var num2 = 2000;
+
+// var sum;
+
+// sum = num1 + num2;
+// console.log(sum)
