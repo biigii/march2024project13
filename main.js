f7bb2610c9184b90485943fdbbab0886b61e42b5
Behshad <82580731+biigii@users.noreply.github.com>
2024-04-16 20:24:14 +1200

    click and function

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+// document.querySelector('h1').style.backgroundColor = "red"
+// document.querySelector(".heading").style.backgroundColor = "red"
+// document.querySelector('#heading').style.backgroundColor = "yellow"
 
+// here is a function which adds an image to html when button clicked
+function addImageToMyPage() {
+    var image = document.createElement("img")
+    image.setAttribute("src", "https://picsum.photos/id/1060/200")
+    image.setAttribute("alt", "this image is being added to html via JS")
+    image.classList.add('my-photo')
+    document.body.appendChild(image);
+}
 
+document.querySelector("#add-image").addEventListener("click",addImageToMyPage)
 
-var mainHeading = document.createElement("h1");
-mainHeading.textContent = "homepage"
-document.body.appendChild(mainHeading);
+function myFirstFunction() {
+   document.querySelector("h1").style.color = "red"
+   document.querySelector("h1").style.textAlign = "center"
+   document.body.style.backgroundColor = "#f99"
+}
 
-var image = document.createElement('img')
-image.src = "https://picsum.photos/id/1050/300"
-image.alt = "my very awesome photo"
-image.style.borderRadius = "50%"
-image.style.border = "3px solid"
-document.getElementById("image-holder").appendChild(image)
+document.querySelector("#btn").addEventListener("click",myFirstFunction)
